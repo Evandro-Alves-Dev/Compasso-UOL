21a3ed53ac4542927ff7ffd73d25531a81dccd2a
Evandro-Alves-Dev <70543731+Evandro-Alves-Dev@users.noreply.github.com>
2021-12-11 16:45:57 -0300

    Aumento de cobertura dos testes unitarios.

--- src/test/java/com/uol/compasso/API/service/ProductServiceTest.java
+++ src/test/java/com/uol/compasso/API/service/ProductServiceTest.java
@@ -6,20 +6,19 @@ import com.uol.compasso.API.entity.Product;
 import com.uol.compasso.API.mapper.MapperProductRequestToProduct;
 import com.uol.compasso.API.mapper.MapperProductToProductResponse;
 import com.uol.compasso.API.repository.ProductRepository;
-import org.hibernate.procedure.spi.ParameterRegistrationImplementor;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ProductServiceTest {
@@ -39,10 +38,17 @@ public class ProductServiceTest {
     @Autowired
     private ProductRequest productRequest;
 
+    Long id;
+
+    Optional<Product> product;
+
     @Before
     public void setUp(){
         productRequest = new ProductRequest("Teclado",
-                "Produto para inderir informações ao computador", new BigDecimal("149.00"));
+                "Produto para inserir informações ao computador", new BigDecimal("149.00"));
+        id = 1L;
+        product = Optional.of(new Product(1L, "Tecaldo novo",
+                "Produto para inserir informações ao computador", new BigDecimal("299.00")));
     }
 
     @Test
@@ -52,5 +58,14 @@ public class ProductServiceTest {
         when(mapperProductToProductResponse.toDto(any())).thenReturn(new ProductResponse());
 
         this.productService.cadastrarProduto(productRequest);
+    }
+
+    @Test
+    public void alterarProdutoTest(){
+        when(productRepository.findById(id)).thenReturn(product);
+        when(productRepository.save(any())).thenReturn(new Product());
+        when(mapperProductToProductResponse.toDto(any())).thenReturn(new ProductResponse());
+
+        this.productService.alterarProduto(id, productRequest);
     }
 }
